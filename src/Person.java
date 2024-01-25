e1a14fcd6653a98e9b8933fce2cf8762ff8f8577
ytpme <153881028+ytpm3@users.noreply.github.com>
2024-01-25 23:52:19 +0600

    Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,59 +1,53 @@
-public class Person implements Payable, Comparable<Person> {
-    public static int counter = 1;
+class Person implements Comparable<Person> {
+    private static int idCounter = 1;
+
     private int id;
     private String name;
     private String surname;
 
     public Person() {
-        this.id = counter++;
+        this.id = idCounter++;
     }
 
     public Person(String name, String surname) {
-        this.id = counter++;
+        this();
         this.name = name;
-        this.surname = surname;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public void name(String name) {
-        this.name = name;
-    }
-
-    public String surname() {
-        return surname;
-    }
-
-    public void surname(String surname) {
         this.surname = surname;
     }
 
     @Override
     public String toString() {
-        return "Person [id=" + id + ", name=" + name + ", surname=" + surname + "]";
+        return String.format("%s: %d %s %s", getPosition(), id, name, surname);
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getPosition() {
-        return "Student";
+        return "Person";
     }
 
     @Override
+    public int compareTo(Person otherPerson) {
+        return Double.compare(this.getPaymentAmount(), otherPerson.getPaymentAmount());
+
+    }
+
+
     public double getPaymentAmount() {
         return 0.00;
-    }
-
-    @Override
-    public int compareTo(Person person) {
-        return 0;
     }
 }
