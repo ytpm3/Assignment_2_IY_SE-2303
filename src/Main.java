441427bc50faaa8da04cdedc5092d87f483634a8
ytpme <153881028+ytpm3@users.noreply.github.com>
2024-01-25 22:13:55 +0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> people = new ArrayList<>();
+
+        Employee employee1 = new Employee("Miras", "Askarov", "Manager", 32045.73);
+        Employee employee2 = new Employee("Ansar", "Muratov", "Developer", 60000.00);
+        Student student1 = new Student("Argiza", "Yerzhanova", 0.00);
+        Student student2 = new Student("Sergey", "Stasov", 36660.00);
+
+        people.add(student1);
+        people.add(employee1);
+        people.add(student2);
+        people.add(employee2);
+
+
+        Collections.sort(people);
+
+        printData(people);
+    }
+
+    public static void printData(Iterable<Person> people) {
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
     }
 }
