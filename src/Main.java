1c92bcd371f5f6a7b5e3f4d5c8191eb1e2ce4dd7
ytpme <153881028+ytpm3@users.noreply.github.com>
2024-01-25 23:50:34 +0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,27 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-
-public class Main {
+public class main {
     public static void main(String[] args) {
-        List<Person> people = new ArrayList<>();
+        Employee employee1 = new Employee("Miras", "Askarov", "Manager", 25000.00);
+        Employee employee2 = new Employee("Ansar", "Muratov", "Developer", 50000.00);
+        Student student1 = new Student("Argiza", "Yerzhanova", 3.50);
+        Student student2 = new Student("Sergey", "Stasov", 2.20);
 
-        Employee employee1 = new Employee("Miras", "Askarov", "Manager", 32045.73);
-        Employee employee2 = new Employee("Ansar", "Muratov", "Developer", 60000.00);
-        Student student1 = new Student("Argiza", "Yerzhanova", 0.00);
-        Student student2 = new Student("Sergey", "Stasov", 36660.00);
-
-        people.add(student1);
-        people.add(employee1);
-        people.add(student2);
-        people.add(employee2);
+        ArrayList<Person> peopleList = new ArrayList<>();
+        peopleList.add(employee1);
+        peopleList.add(employee2);
+        peopleList.add(student1);
+        peopleList.add(student2);
 
 
-        Collections.sort(people);
 
-        printData(people);
+        Collections.sort(peopleList);
+
+        printData(peopleList);
     }
-
     public static void printData(Iterable<Person> people) {
         for (Person person : people) {
-            System.out.println(person.toString());
+            System.out.printf("%s earns %.2f tenge%n", person, person.getPaymentAmount());
         }
     }
 }
